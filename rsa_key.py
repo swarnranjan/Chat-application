ddf397b0d3876d10a40d07ef3dc26d38ee4c7065
swarnranjan <swarnranjan.2004@gmail.com>
2025-06-15 23:29:08 +0530

    name changes

--- rsa_key.py
+++ rsa_key.py
@@ -1,13 +1,13 @@
 from Crypto.PublicKey import RSA
 
-username = "swarn"
+username = "John"
 key = RSA.generate(2048)
 with open(f"private_{username}.pem", "wb") as priv:
     priv.write(key.export_key())
 with open(f"public_{username}.pem", "wb") as pub:
     pub.write(key.publickey().export_key())
 
-username = "kamakhya"
+username = "Mary"
 key = RSA.generate(2048)
 with open(f"private_{username}.pem", "wb") as priv:
     priv.write(key.export_key())
